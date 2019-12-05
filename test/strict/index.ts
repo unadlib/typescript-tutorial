c94449bec9ae73cc38de0b8994d39bf9788079e2
Michael Lin <unadlib@gmail.com>
2019-12-05 23:59:57 +0800

    update

--- test/strict/index.ts
+++ test/strict/index.ts
@@ -100,17 +100,3 @@ namespace Assertions {
     //
   }
 }
-
-// function isString(test: any): test is string{
-//   return typeof test === 'string';
-// }
-
-// function example(foo: any){
-//   if (isString(foo)) {
-//       let _foo = foo as string;
-//       console.log(_foo.length);  // string function
-//       console.log(foo.length); // string function
-//   }
-// }
-// example('hello world');
-
